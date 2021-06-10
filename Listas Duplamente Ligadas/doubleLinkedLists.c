47c42f812d6563887ebcd6058edae96368f4d0e2
Pedro Simões <pvsimoes88@gmail.com>
2021-06-10 22:58:09 +0100

    Update doubleLinkedLists.c

--- Listas Duplamente Ligadas/doubleLinkedLists.c
+++ Listas Duplamente Ligadas/doubleLinkedLists.c
@@ -55,7 +55,7 @@ List* sorted_insert(List* list , int value) {
     else {
         List *aux = list ;
 
-        for (; aux->next && aux->next->n < value; aux = aux->next ) { }
+        for (; aux->next && aux->next->n <= value; aux = aux->next ) { }
 
         new_node->next = aux->next ;
         new_node->prev = aux ;
@@ -66,7 +66,52 @@ List* sorted_insert(List* list , int value) {
 
     return list;
 }
+
 #pragma endregion
+
+#pragma region search/print
+
+void show_linked_list(List* list){
+    for(List* aux = list; aux; aux = aux->next)
+        printf("-> %d\n", aux->n);
+}
+
+#pragma endregion
+
+#pragma region Free
+
+void destroyList(List* list){
+    List *aux;
+
+    for(; list; list = list->next){
+        aux = list->next;
+        free(list);
+    }
+}
+
+List* removeElement(List *list, int v){
+    int removido = 0;
+
+    for (List* aux = list; aux && removido == 0; aux = aux->next)
+    {
+        if(aux->n == v){
+            if(aux->next)
+                aux->next->prev = aux->prev;
+            if(aux->prev)
+                aux->prev->next = aux->next;
+            else
+                list = aux->next;
+            free(aux);
+            //APENAS USAR REMOVIDO CASO QUEIRA REMOVER APENAS O PRIMEIRO NODO COM O VALOR v, PARA REMOVER TODOS, TIRAR VARIAVEL
+            removido = 1;
+        }
+    }
+    
+    return list;
+}
+
+#pragma endregion
+
 int main(){
     List* list = NULL ;
 
